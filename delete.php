92633b38449e18cd949684d9a9e5544be66562c5
DanaAllouche <138357209+DanaAllouche@users.noreply.github.com>
2023-07-18 15:04:43 +0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,15 +1,31 @@
 <?php
 include 'connect.php';
-if (isset ($_GET['deleteid'])){
-    $id= $_GET['deleteid'];
+if (isset($_POST['book_id'])){
+    $id = $_POST['book_id'];
 
-    $sql = "delete from  `books` where id=$id";
-    $result=mysqli_query($conn,$sql);
+    $sql = "DELETE FROM `books` WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
     if ($result){
-        // echo "Delete successful";
-        header ('location:display.php');
+        header('Location: delete.php');
+        exit();
     } else{
         die(mysqli_error($conn));
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Delete Book</title>
+    <link rel="stylesheet" href="../SikkaLibrary/style.css">
+</head>
+<body>
+    <h1>Delete Book</h1>
+    <form action="delete_book.php" method="POST">
+        <label for="book_id">Book ID:</label>
+        <input type="text" id="book_id" name="book_id" required>
+        <input type="submit" value="Delete">
+    </form>
+</body>
+</html>
