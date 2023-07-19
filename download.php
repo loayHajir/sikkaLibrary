2d4fdeb37e5cbc09baf7b0bc8da6e624c6952bb3
Dana Allouche <danaallouche2016@gmail.com>
2023-07-19 15:43:18 +0300

    Add download.php file for handling file downloads

---
+++ download.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_GET['file'])) {
+    $file = $_GET['file'];
+    $file = urldecode($file);
+    $filePath = 'uploads/' . $file;
+
+    if (file_exists($filePath)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="' . basename($filePath) . '"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filePath));
+        readfile($filePath);
+        exit;
+    } else {
+        echo 'File not found.';
+    }
+}
+?>
