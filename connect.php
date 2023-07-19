fdeabefc1d97937ab52e84c870a536cdf621bfa1
Dana Allouche <danaallouche2016@gmail.com>
2023-07-19 11:58:30 +0300

    bbbbbbbbbb

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 $serverName = 'localhost';
 $userName = 'root';
 $password = '';
-$db = 'sikkalibrary';
+$db = 'sikka library';
 $conn = mysqli_connect($serverName, $userName, $password,$db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
