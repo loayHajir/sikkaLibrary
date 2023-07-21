c55439e20e7749e9e85748da5da7f70289cc7e28
Loay <loayhajir122@gmail.com>
2023-07-21 12:53:56 +0300

    sql update

--- sikka_library.sql
+++ sikka_library.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2023 at 12:17 AM
+-- Generation Time: Jul 21, 2023 at 11:52 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,8 +41,33 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`ID`, `BookName`, `Description`, `Language`, `Available`, `PDF`) VALUES
-(24, 'Java', 'JavaMaster', 'English', 0, 'Dana Allouche CV-Updated.pdf'),
-(25, 'j', 'bkjh', 'English', 1, 'Dana Allouche CV-Updated.pdf');
+(55, 'book2 ', 'this is second test', 'English', 1, 'uploads/1036971388-id1.pdf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `phoneno` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `Type` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`ID`, `username`, `password`, `phoneno`, `email`, `dob`, `gender`, `Type`) VALUES
+(1, 'loay', 'loay123', '81468412', 'loayhajir122@gmail.com', '2002-07-19', 'Male', 'Admin'),
+(2, 'zeinab', 'zeinab123', '81749034', 'zeinabmiari4@gmail.com', '2002-08-20', 'female', 'user'),
+(3, 'lyal', 'lyal123', '81468412', 'layalk@gmail.com', '1986-03-03', 'Female', '');
 
 --
 -- Indexes for dumped tables
@@ -55,6 +80,12 @@ ALTER TABLE `books`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -62,7 +93,13 @@ ALTER TABLE `books`
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
