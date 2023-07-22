750308fea5738f47bff07b66565378df648475ec
Loay <loayhajir122@gmail.com>
2023-07-22 11:55:13 +0300

    look for live config

--- config.php
+++ config.php
@@ -1,13 +1,17 @@
 <?php
 
-$serverName = 'localhost';
-$userName = 'root'; 
+$serverName = '';
+$userName = '';
 $password = '';
-$db = 'sikka library';
+$db = '';
 
 
 // To create your own variables related to you own environment. Copy this file to `config.local.php` and set your variables there.
 // This file will be added to .gitignore and used locally to set your own config variables without affecting the live settings
 if (file_exists('config.local.php')) {
   include 'config.local.php';
+}
+
+if (file_exists('config.live.php')) {
+  include 'config.live.php';
 }
