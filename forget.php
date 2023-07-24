258cbfbc6c7e65c72b884fc5333e37231d2b6b82
Zeinab Miari <zeinabb.miarii@gmail.com>
2023-07-24 15:04:52 +0300

    forget password update

--- forget.php
+++ forget.php
@@ -1,18 +1,20 @@
 <?php
 include 'connect.php';
-if (isset($_POST['Submit'])) {
-$name = $_POST['username'];
-$question = $_POST['question'];
-$answer = $_POST['answer'];
+session_start();
 
-$query = mysqli_query($conn, "select * from login where username = '$name' && questions = '$question' && answer =$'$answer'");
-$result = mysqli_num_rows($query);
-$row = mysqli_fetch_array($query);
-if (!$resutl) {
-    echo "username or question or answer incorrect";
-} else {
-    header("Location: resetpass.php");
-}
+if (isset($_POST['Submit'])) {
+    $name = $_POST['username'];
+    $question = $_POST['question'];
+    $answer = $_POST['answer'];
+
+    $query = mysqli_query($conn, "select * from `login` where username = '$name' && questions = '$question' && answer = '$answer'");
+    $result = mysqli_num_rows($query);
+
+    if (!$result) {
+        echo "Username or question or answer incorrect";
+    } else {
+        header("location: resetpass.php");
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -71,23 +73,25 @@ if (!$resutl) {
   </style>
 </head>
 <body>
-  <form action="#" method="post">
+  <form action="resetpass.php" method="post">
     <label for="username">Username:</label>
     <input type="text" id="username" name="username" required>
 
-    <label for="question">Select a question:</label>
-    <select id="question" name="question" required>
+    <!-- <label for="question">Select a question:</label> -->
+    <!-- <select id="question" name="question" required>
       <option value="" disabled selected>Select a question</option>
       <option value="q1">What is your favorite color</option>
       <option value="q2">What is your favorite movie</option>
       <option value="q3">What city were you born in</option>
-      <!-- Add more options as needed -->
-    </select>
+    </select> -->
+
+    <label for="quse">question:</label>
+    <input type="text" id="question" name="question" required>
 
     <label for="answer">Answer:</label>
     <input type="text" id="answer" name="answer" required>
 
-    <input type="submit" value="Submit" formaction="resetpass.php">
+    <input type="submit" value="Submit" name="Submit" >
   </form>
 </body>
 </html>
