c606b0a5ad518a15a6e5d59fa0b2bf4bea89a850
Zeinab Miari <zeinabb.miarii@gmail.com>
2023-08-28 11:25:43 +0300

    add desc

--- core/config.php
+++ core/config.php
@@ -3,7 +3,7 @@
 $serverName = 'localhost';
 $userName = 'root';
 $password = '';
-$db = 'sikka library';
+$db = 'sikkalibrary';
 
 // To create your own variables related to you own environment. Copy this file to `config.local.php` and set your variables there.
 // This file will be added to .gitignore and used locally to set your own config variables without affecting the live settings
